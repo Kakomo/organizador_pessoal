266f3497e915ea1b90a8525328135aa18d204d3e
Kako <kakomo2@gmail.com>
2022-04-22 09:13:18 -0300

    Aula 5
    Ajustes Finais e novo Layout

--- lib/screens/tela_inicial.dart
+++ lib/screens/tela_inicial.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:organizador_pessoal/components/tasks.dart';
 
-import '../components/tarefa.dart';
 class TelaInicial extends StatefulWidget {
   const TelaInicial({Key? key}) : super(key: key);
 
@@ -16,58 +16,41 @@ class _TelaInicialState extends State<TelaInicial> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter: Primeiros Passos'),
-        leading: Container(
-          decoration: BoxDecoration(
-            border: Border.all(width: 2),
-            color: Colors.black26,
-          ),
-          child: Image.network(
-            'https://i.ibb.co/cctxpTG/kako-epifania-2022-18-2.jpg',
-            fit: BoxFit.cover,
-          ),
-        ),
+        leading: Icon(Icons.add_task),
       ),
       body: AnimatedOpacity(
         opacity: opacidade ? 1.0 : 0.0,
         duration: const Duration(milliseconds: 500),
-        child: ListView(
-          children:  const [
-            Tarefas(
-                'Estudar Flutter',
-                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                'PC',
-                2,
-                0.5),
-            Tarefas(
-                'Estudar Flutter',
-                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                'PC',
-                2,
-                1),
-            Tarefas(
-                'Estudar Flutter',
-                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                'PC',
-                2,
-                0.1),
-            Tarefas(
-                'Estudar Flutter',
-                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                'PC',
-                2,
-                0.1),
-            Tarefas(
-                'Estudar Flutter',
-                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                'PC',
-                2,
-                0.1),Tarefas(
-                'Estudar Flutter',
-                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                'PC',
-                2,
-                0.1),
-          ],
+        child: Container(
+          color: Color.fromARGB(255, 208, 221, 237),
+          child: ListView(
+            children: const [
+              Padding(//aula 5.4
+                padding: EdgeInsets.only(top:8),
+                child: Tasks(
+                    'Estudar Flutter',
+                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                    3),
+              ),
+              Tasks(
+                  'Andar de Bike',
+                  'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg',
+                  2),
+              Tasks(
+                  'Ler 50 páginas',
+                  'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
+                  1),
+              Tasks(
+                  'Meditar',
+                  'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
+                  4),
+              Tasks('Jogar',
+                  'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg', 0),
+              SizedBox(
+                height: 100,
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -76,7 +59,8 @@ class _TelaInicialState extends State<TelaInicial> {
             opacidade = !opacidade;
           });
         },
-        child: const Icon(Icons.add),
+        backgroundColor: Colors.blue[100],
+        child: const Icon(Icons.remove_red_eye),
       ),
     );
   }
